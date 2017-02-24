431984029faa39b63b107f159c12bfebcede8f27
mariomc2 <mariomc2@users.noreply.github.com>
2017-02-24 10:31:11 -0600

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  
+  root "hablas#index"
+  
   resources :hablas
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
